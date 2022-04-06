d9c80f10b8ad34e4d1a2599ac946047c629e3687
siri16-p <siri16.qa@gmail.com>
2022-04-06 14:38:32 +0530

    Added Automation test suite for the requirement.

---
+++ src/test/java/com/driver/Driver.java
@@ -0,0 +1,40 @@
+package com.driver;
+
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.utilities.ReadPropertiesFile;
+
+public class Driver {
+	public static WebDriver driver = null;
+	public static final String filename = null;
+	public ReadPropertiesFile readfile = new ReadPropertiesFile();
+	public Properties prop = readfile.readPropertiesFile(filename);
+
+	public static void init(String browser) {
+		if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+		}
+	}
+	
+	public static String LinkUrl(WebDriver driver,String linkText) {
+		String s="";
+		try {
+			s=driver.findElement(By.linkText(linkText)).getAttribute("href");
+		} catch(NoSuchElementException e) {
+			return null;
+		}
+		return s;
+	}
+}
